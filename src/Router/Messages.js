d5fece081fbc3e813aaa3ba430d3d14d35079666
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-08-22 18:53:38 +0530

    url update and check process 4

--- src/Router/Messages.js
+++ src/Router/Messages.js
@@ -1,98 +1,83 @@
-import React, { useEffect, useState,useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import { FriendsRoute, url } from '../utils/Routes';
 import AllFriends from '../Components/Friends';
 import Welcome from '../Components/welcome';
 import ChatBox from '../Components/ChatBox';
-import {io}from 'socket.io-client'
+import { io } from 'socket.io-client';
 
-
-
-function Messages() {
+const Messages = () => {
   const history = useHistory();
   const socket = useRef();
-  const[Friends, setFriends] = useState([])
-  const[currentUser, setCurrentUser] = useState(undefined)
-  const[currentChat, setCurrentChat]= useState(undefined)
-  const[loaded, setLoaded] = useState(false)
-  const[connection,setConnection]=useState(false)
-  useEffect(()=>{
-    async function fetchedData(){
-      
-         const data = await JSON.parse(localStorage.getItem('ChitChatz-user'));
-         if(!data){
-          history.push('/')
-         }else{
-          setCurrentUser(data);
-          setLoaded(true)
-         }
-         
-        
+  const [Friends, setFriends] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    async function fetchedData() {
+      const data = await JSON.parse(localStorage.getItem('ChitChatz-user'));
+      if (!data) {
+        history.push('/');
+      } else {
+        setCurrentUser(data);
+        setLoaded(true);
+      }
     }
     fetchedData();
+  }, []);
 
-  },[])
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(url, {
+        withCredentials: true,
+        transports: ['websocket', 'polling'],
+        upgrade: false,
+        extraHeaders: {
+          'my-custom-header': 'abcd'
+        }
+      });
+      socket.current.emit('add-user', currentUser._id);
 
-useEffect(()=>{
-  if(currentUser){
-    socket.current = io('wss://chitchatzapp.netlify.app',{
-      withCredentials: true,
-      transports: ["websocket","polling"],
-      upgrade:false,
-      extraHeaders: {
-        "my-custom-header": "abcd"
-      }
-    })
-    socket.current.emit("add-user", currentUser._id)
- }
-},[currentUser])
-
-
+      return () => {
+        if (socket.current) {
+          socket.current.disconnect();
+        }
+      };
+    }
+  }, [currentUser]);
 
   useEffect(() => {
     async function fetchData() {
-      if (currentUser) {
-        try {
-          const data = await axios.get(`${FriendsRoute}/${currentUser._id}`)
-          // const response = await fetch(`${FriendsRoute}/${currentUser._id}`,{
-          //   method:'GET',
-          //   body:JSON.stringify(),
-          //   headers:{
-          //     'content-type':'application/json'
-          //   }
-          // })
-          // const data = await response.json()
-          console.log(currentUser._id);
-          setFriends(data.data)
-        } catch (error) {
-          console.log(error.message);
-        }
+      try {
+        const response = await axios.get(`${FriendsRoute}/${currentUser._id}`);
+        setFriends(response.data);
+      } catch (error) {
+        console.log('Error fetching friends:', error);
       }
     }
-    fetchData();
+    if (currentUser) {
+      fetchData();
+    }
   }, [currentUser]);
 
-
-const handleChatChange= (chat)=>{
-setCurrentChat(chat);
-}
-
-
-
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
 
   return (
-    <div id='message-sec'>
-<div className='page'>
-  <AllFriends Friends={Friends} currentUser={currentUser} changeChat={handleChatChange} />
-{loaded && currentChat===undefined?   (<Welcome currentUser={currentUser}  />): (<ChatBox currentChat={currentChat} currentUser={currentUser} socket={socket}/>)
-}
-
-</div>
+    <div id="message-sec">
+      <div className="page">
+        <AllFriends Friends={Friends} currentUser={currentUser} changeChat={handleChatChange} />
+        {loaded && currentChat === undefined ? (
+          <Welcome currentUser={currentUser} />
+        ) : (
+          <ChatBox currentChat={currentChat} currentUser={currentUser} socket={socket} />
+        )}
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-
-export default Messages
+export default Messages;
