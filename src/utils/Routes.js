e87c09b2db5f41b2df5859cf03a07a3038f5bb3a
VIGNESH G <118879122+vigneshdevelopr@users.noreply.github.com>
2024-01-07 20:23:44 +0530

    Update Routes.js

--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -1,5 +1,5 @@
 // export const url = "https://chitchatz.onrender.com"
-export const url = 'https://chitchatzserver.up.railway.app'
+export const url = 'https://chit-chatz-app-project-backend.onrender.com'
 export const signupRoute = `${url}/api/auth/signup`;
 export const loginRoute = `${url}/api/auth/login`;
 export const FriendsRoute = `${url}/api/auth/friends`; 
